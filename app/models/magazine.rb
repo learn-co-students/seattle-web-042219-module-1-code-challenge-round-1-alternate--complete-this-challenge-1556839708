70aeb3c906358754b56a2ce752db3c3fd80a3bc8
Hanna Engel <hengel@alumni.berklee.edu>
2019-05-02 17:37:38 -0700

    done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,32 @@
 class Magazine
-  attr_accessor :name, :category
+attr_accessor :name, :category
 
+@@all = []
 
-  def initialize(name, category)
-    @name = name
-    @category = category
+def initialize (name, category)
+@name = name
+@category = category
+@@all<< self
+end
 
-  end
+def self.all
+@@all
+end
 
+def self.find_by_name(mag)
+@@all.find{|magazine| magazine.name == mag}
+end
+
+def article_titles
+Article.all.map{|article|article.title if article.magazine == self}
+end
+
+def contributors
+Article.all.map{|article|article.author if article.magazine == self}
+end
+
+def word_count
+Article.all.map{|article|article.content if article.magazine ==self}.compact.join.split.size
+end
 
 end
